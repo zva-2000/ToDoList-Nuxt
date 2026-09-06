dda6d4c29d833bc2d85008291519b4f5ca65f8e9
zva-2000 <zva-2000@mail.ru>
2026-09-06 12:58:46 +0300

    Добавила базовые стили

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/scss/main.scss']
 })
